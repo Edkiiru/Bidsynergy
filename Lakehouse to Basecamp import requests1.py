aefcd4b8091bac67ade505d451c36c9cbebd71ae
Edkiiru <24961321+Edkiiru@users.noreply.github.com>
2025-01-13 14:29:27 -0600

    Update Lakehouse to Basecamp import requests1.py

--- Lakehouse to Basecamp import requests1.py
+++ Lakehouse to Basecamp import requests1.py
@@ -3,10 +3,10 @@ import base64
 import os
 
 # API endpoint URL with query string parameter
-url = 'https://api.retool.com/v1/workflows/1ce3b659-ec4a-4596-a4a1-95fa5ce2517d/startTrigger?workflowApiKey=retool_wk_c53e4448bd9a460fa3130f234b01ab15'
+url = 'https://api.<xxx>'
  
 #Path to the CSV file in the lakehouse
-csv_path = '/lakehouse/default/Files/Filtered_Csv/part-00000-dca0156f-e89f-481d-b5b0-1c305f72bcca-c000.csv'
+csv_path = '<xxx.csv>'
 
 # Verify the file exists in the expected location
 if os.path.isfile(csv_path):
@@ -19,7 +19,7 @@ if os.path.isfile(csv_path):
 
         # Define the data to be sent
         data = {
-            'name': 'part-00000-dca0156f-e89f-481d-b5b0-1c305f72bcca-c000.csv',
+            'name': 'xxxx.csv',
             'type': 'text/csv',
             'base64Data': encoded_csv  # Add your base64 encoded data here
         }
